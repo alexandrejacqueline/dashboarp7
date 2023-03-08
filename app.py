a7ced6516e365e264ea8b94e9b530ae106609811
alexandrejacqueline <68188767+alexandrejacqueline@users.noreply.github.com>
2023-03-08 21:59:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -25,22 +25,7 @@ from sklearn.metrics import plot_precision_recall_curve
 #import databases
 import sqlalchemy
 st.set_page_config(page_title = "Multipage App")
-#DATABASE_URL = "sqlite:///./test.db"
 
-"""host_server = os.environ.get('host_server', 'localhost')
-db_server_port = urllib.parse.quote_plus(str(os.environ.get('db_server_port', '5432')))
-database_name = os.environ.get('database_name', 'fastapi')
-db_username = urllib.parse.quote_plus(str(os.environ.get('db_username', 'postgres')))
-db_password = urllib.parse.quote_plus(str(os.environ.get('db_password', 'secret')))
-ssl_mode = urllib.parse.quote_plus(str(os.environ.get('ssl_mode','prefer')))
-DATABASE_URL = 'postgresql://{}:{}@{}:{}/{}?sslmode={}'.format(db_username, db_password, host_server, db_server_port, database_name, ssl_mode)
-
-
-engine = sqlalchemy.create_engine(
-    #DATABASE_URL, connect_args={"check_same_thread": False}
-    DATABASE_URL, pool_size=3, max_overflow=0
-)
-metadata.create_all(engine)"""
 
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
